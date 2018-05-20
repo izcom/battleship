606ff9830c01535636e26cf4eca2b1d5aa094351
Darian Garcia <Garcia.Darian.Lee@gmail.com>
2018-05-19 19:34:07 -0600

    Allows printing of the board, WIP - assigning array of hashes with instances of Space as keys

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,21 @@
 require 'pry'
+require './lib/space'
 
 class Board
 
   def initialize
-    @rows = []
+    @rows = [[],[],[],[]]
     @rows = assign_hash_to_board
   end
 
   # creates hashes with nils A[1] => nil, A[2] => nil...B1 => nil...etc
   def assign_hash_to_board
-    4.times.with_index(offset = 1) do |row_number|
-      4.times.with_index(offset = 1) do |index|
-      symbol_identifier = 'A' + [index].to_s
-      "#{symbol_identifier = Hash.new}"
-      [:"#{symbol_identifier}"] = nil
-      @rows[row_number] << "#{symbol_identifier}"
-
-      end
+    @rows.each.with_index(offset = 1) do |index|
+      @rows = [
+        { :A => {:A[index] => Space.new},
+          :B => {:B[index] => Space.new},
+          :C => {:C[index] => Space.new},
+          :D => {:D[index] => Space.new} }]
     end
   end
 
@@ -24,14 +23,18 @@ class Board
     puts `clear`
     print "===========\n"
     print ". 1 2 3 4\n"
-    loop.with_index do |index|
-      ascii_index = 41 # 'A'
-      binding.pry
-    print (ascii_index + index).chr
-    end
-    loop.with_index do |index|
-      print :A[index]
-    end
+    draw_board_spaces
     print '==========='
+  end
+
+  def draw_board_spaces
+    ascii_index = 65 # 'A'
+    4.times.with_index(offset = 0) do |index|
+      ascii_char = (ascii_index + index).chr
+      4.times.with_index(offset = 1) do |count|
+        print :"#{ascii_char}"[count]
+      end
+      print "\n"
+    end
   end
 end
